9c8a821bd6e9f5e0d35149b8b4e518f267083405
ozcankursun <62161820+ozcankursun@users.noreply.github.com>
2021-05-18 20:34:09 +0300

    second

---
+++ react_course/src/components/MetricItem.js
@@ -0,0 +1,15 @@
+import "./MetricItem.css"
+
+const MetricItem = () =>    {
+
+    return <div className="metric-item">
+    <div className="metric-item_date">11/5 17:30</div>
+    <div className="metric-item_metric">
+    <div>Vassiliki D</div>
+    <h3>StockMarket</h3>
+    </div>
+    <div className="metric-item_count">16</div>
+    </div>
+
+}
+export default MetricItem
